13570b7a4488b29a29263a10312143390948b8ab
Mathieu Ibersien <mat98-76@live.fr>
2021-07-06 22:21:27 +0200

    Dynamic selection card and description

--- main.js
+++ main.js
@@ -10,13 +10,27 @@ one.addEventListener("mouseover", (e) => {
 */
 
 const cards = document.querySelectorAll('.card');
-console.log(cards)
+const description = document.querySelector('.description');
 let selectedElement = null;
 
-cards.forEach(elem => {
-    elem.addEventListener('click', e => {
+const descriptions = [
+    "Gaumont is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>",
+    "coucoucou"
+]
+
+let cardsList = [];
+cards.forEach((card, index) => {
+    cardsList.push({ index: index, card: card, value: descriptions[index]} );
+})
+
+cardsList.forEach(elem => {
+    elem.card.addEventListener('click', e => {
         selectedElement != null && selectedElement.classList.remove('selected');
-        elem.classList.add('selected');
-        selectedElement = elem;
+        elem.card.classList.add('selected');
+        selectedElement = elem.card;
+
+        description.innerHTML = elem.value
     })
 })
+
+
