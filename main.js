0e2d1b167cae162b5b9a71f4e540dcf77e00f778
Mathieu Ibersien <mat98-76@live.fr>
2021-07-08 22:20:28 +0200

    Add a secon project

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let selectedElement = null;
 
 const descriptions = [
     "Gaumont is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>",
-    "coucoucou"
+    "Immob Announce is a project made in my 2nd year of MSc. It reproduces the concept of the famous website: Leboncoin.fr. Classic CRUD ad management for super user and Q&A possibility for the \"client\". Immob Annouce is also able to manage image files thanks to file thank to multer library. This project run with a Node Express server with a MongoDB and expose html template with pug technology."
 ]
 
 let cardsList = [];
