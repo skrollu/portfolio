eccc1977d1224163df561456b938d37f1169b88f
Mathieu Ibersien <mat98-76@live.fr>
2021-07-09 20:22:30 +0200

    Ux improved, inner card removed, mouseover card and help box size and time changed

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 gsap.registerPlugin(TextPlugin)
 gsap.registerPlugin(ScrollTrigger)
 
-/*
-const one = document.querySelector('#one')
-
-one.addEventListener("mouseover", (e) => {
-    var popDesc = document.createElement("div");
-    popDesc.setAttribute('class', 'pop-desc')
-    popDesc.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px;`)
-    one.appendChild(popDesc)
-})
-*/
-
 const cards = document.querySelectorAll('.card');
 const description = document.querySelector('.description');
+const hoveredCard = document.querySelector('.hovered-card')
 let selectedElement = null;
 
 const descriptions = [
-    "Gaumont is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>",
-    "Immob Announce is a project made in my 2nd year of MSc. It reproduces the concept of the famous website: Leboncoin.fr. Classic CRUD ad management for super user and Q&A possibility for the \"client\". Immob Annouce is also able to manage image files thanks to file thank to multer library. This project run with a Node Express server with a MongoDB and expose html template with pug technology.",
-    "Highly inspired VueX project to try what's in my mind on new technologies to me."
+    { title: "Gaumont Angular", description: "<a href=\"https://github.com/skrollu/angular-gaumont\">Gaumont</a> is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>"},
+    { title: "Immob Annouce",  description: "<a href=\"https://github.com/skrollu/angular-gaumont\">Immob Announce</a> is a project made in my 2nd year of MSc. It reproduces the concept of the famous website: Leboncoin.fr. Classic CRUD ad management for super user and Q&A possibility for the \"client\". Immob Annouce is also able to manage image files thanks to file thank to multer library. This project run with a Node Express server with a MongoDB and expose html template with pug technology." },
+    { title: "Vuex", description: "<a href=\"https://github.com/skrollu/angular-gaumont\">Vuex-tuto</a> is a highly inspired by youtube tutorials project to try what's in my mind on new technologies to me."}
 ]
 
 let cardsList = [];
@@ -28,12 +18,13 @@ cards.forEach((card, index) => {
 })
 
 cardsList.forEach(elem => {
-    elem.card.addEventListener('click', e => {
+    elem.card.addEventListener('mouseover', e => {
         selectedElement != null && selectedElement.classList.remove('selected');
         elem.card.classList.add('selected');
         selectedElement = elem.card;
 
-        description.innerHTML = elem.value
+        hoveredCard.innerHTML = elem.value.title
+        description.innerHTML = elem.value.description
     })
 })
 
@@ -48,11 +39,11 @@ const timeline = gsap.timeline({
     }
 });
 
-const textHelperWords = "You can click on the different projects to learn more about them...";
+const textHelperWords = "Hovered the different projects to learn more about them...";
 timeline.from(helper, 1, { x: 600, ease: 'power3.inOut'})
         let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1});
         tl.to(".text-helper", {
-                duration: 2,
+                duration: 1.5,
                 text: {
                     value: textHelperWords,
                 },
