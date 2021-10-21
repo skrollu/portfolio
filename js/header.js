a96587d66fe3c454bd939c4fa8ebee45b073e706
Mathieu Ibersien <mat98-76@live.fr>
2021-10-21 22:19:59 +0200

    Ajout des librairies Gsap plutôt que par CDN pour faciliter la gestion de head par swup. Et gestion du mount avec Swup le unmount est a revoir avec les event listener...

--- js/header.js
+++ js/header.js
@@ -1,5 +1,3 @@
-console.log(1)
-
 /**
 gsap.registerPlugin(TextPlugin)
 
--- js/main.js
+++ js/main.js
@@ -1,46 +1,53 @@
-gsap.registerPlugin(TextPlugin)
-gsap.registerPlugin(ScrollTrigger)
+/*
+Paste in transition file directly see module with bundler  if we want to import js file inside them
 
-const cards = document.querySelectorAll('.card');
-const description = document.querySelector('.description');
-const cardTitle = document.querySelector('.card-title')
+export function projectDescriptions() {
+   
+    console.log("test");
 
-const descriptions = [
-    { index: 1, title: "Gaumont Angular", description: "<a class=\"description-link\" href=\"https://github.com/skrollu/angular-gaumont\">Gaumont</a> is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a class=\"description-link\" href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>"},
-    { index: 2, title: "Arduino Lego Controller", description: "<a class=\"description-link\" href=\"https://github.com/skrollu/arduino-mern-lego-controller\">Lego Controller</a> is a project made in IOT class courses during my 2nd year of MSc. It's a FullStack project using React client and Node.js server communicating with an Arduino Uno and a Mongo database. We also a sliding door in Lego combined with a motor branched on the Arduino. A physical button allows us to open/close the door or, we can do it on the React application. Every interaction are register in DB and displayed on and LCD screen and computer screen."},
-    { index: 3, title: "Immob Annouce",  description: "<a class=\"description-link\" href=\"https://github.com/skrollu/angular-gaumont\">Immob Announce</a> is a project made during my 2nd year of MSc. It reproduces the concept of the famous website: Leboncoin.fr. Classic CRUD ad management for super user and Q&A possibility for the \"client\". Immob Annouce is also able to manage image files thanks to file thank to multer library. This project run with a Node Express server with a MongoDB and expose html template with pug technology." }
-];
-
-// list of object: {index, card html element, description}
-let cardsList = [];
-cards.forEach((card, index) => {
-    cardsList.push({ index: index, card: card, value: descriptions[index]} );
-})
-
-function activeCard(element){
-    element.card.classList.add('active');
-    cardTitle.innerHTML = element.value.title
-    description.innerHTML = element.value.description
-}
-
-//By default active the middle card
-activeCard(cardsList[1])
-
-cardsList.forEach(elem => {
-    elem.card.addEventListener('mouseover', e => {
-        //remove active class for every card
-        cardsList.forEach( item => {
-            item.card.classList.remove('active')
+    const cards = document.querySelectorAll('.card');
+    const description = document.querySelector('.description');
+    const cardTitle = document.querySelector('.card-title')
+    
+    const descriptions = [
+        { index: 1, title: "Gaumont Angular", description: "<a class=\"description-link\" href=\"https://github.com/skrollu/angular-gaumont\">Gaumont</a> is a work in progress project to recreate the real Gaumont website in Angular. With it I learned about the leaflet library to display points on a map and interact with them. This project is also where I discovered lots of CSS tricks and my love for styling website. Recently I worked on third party connection with passport.js but I didn't find the time to finish it... Every datas of this project come from my own API-project: <a class=\"description-link\" href=\"https://github.com/skrollu/express-mongo-api\">express-mongo-api</a>"},
+        { index: 2, title: "Arduino Lego Controller", description: "<a class=\"description-link\" href=\"https://github.com/skrollu/arduino-mern-lego-controller\">Lego Controller</a> is a project made in IOT class courses during my 2nd year of MSc. It's a FullStack project using React client and Node.js server communicating with an Arduino Uno and a Mongo database. We also a sliding door in Lego combined with a motor branched on the Arduino. A physical button allows us to open/close the door or, we can do it on the React application. Every interaction are register in DB and displayed on and LCD screen and computer screen."},
+        { index: 3, title: "Immob Annouce",  description: "<a class=\"description-link\" href=\"https://github.com/skrollu/angular-gaumont\">Immob Announce</a> is a project made during my 2nd year of MSc. It reproduces the concept of the famous website: Leboncoin.fr. Classic CRUD ad management for super user and Q&A possibility for the \"client\". Immob Annouce is also able to manage image files thanks to file thank to multer library. This project run with a Node Express server with a MongoDB and expose html template with pug technology." }
+    ];
+    
+    // list of object: {index, card html element, description}
+    let cardsList = [];
+    cards.forEach((card, index) => {
+        cardsList.push({ index: index, card: card, value: descriptions[index]} );
+    })
+    
+    function activeCard(element){
+        element.card.classList.add('active');
+        cardTitle.innerHTML = element.value.title
+        description.innerHTML = element.value.description
+    }
+    
+    //By default active the middle card
+    activeCard(cardsList[1])
+    
+    cardsList.forEach(elem => {
+        elem.card.addEventListener('mouseover', e => {
+            //remove active class for every card
+            cardsList.forEach( item => {
+                item.card.classList.remove('active')
+            });
+            
+            activeCard(elem);
         });
-
-        activeCard(elem);
-    });
-})
-
+    })
+}
+*/
 
 /*
 
 //Helper on scroll
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(TextPlugin)
 
 const helper = document.querySelector('.helper')
 const timeline = gsap.timeline({
