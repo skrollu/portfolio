7021b79efcd166eeb4d0a2e49ef76a4c78c4b77b
Mathieu Ibersien <mathieu.ibersien@etu.univ-lehavre.fr>
2021-10-25 21:48:10 +0200

    add babel config with webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
-    mode: 'production'
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    mode: 'production',
 };
