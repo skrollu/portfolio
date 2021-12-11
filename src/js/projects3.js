0e3eb45f538afaedbd2eb86aaf34cd57a5dda04b
Mathieu Ibersien <mat98-76@live.fr>
2021-12-11 18:16:08 +0100

    section 3 work in progress

--- src/js/projects3.js
+++ src/js/projects3.js
@@ -2,12 +2,12 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 let timelineSectionOne = null
-let timelineSectionTwo = null
 
 export function mountProjects3() {
 	gsap.registerPlugin(ScrollTrigger)
 	mountSectionOne()
-	//mountSectionTwo()
+	mountSectionTwo()
+	mountSectionThree()
 }
 
 function mountSectionOne() {
@@ -25,7 +25,6 @@ function mountSectionOne() {
 			trigger: sectionOne,
 			scrub: 0.5,
 			start: 'top 30%',
-			markers: true,
 		},
 
 		yPercent: 50,
@@ -37,7 +36,6 @@ function mountSectionOne() {
 			trigger: sectionOne,
 			scrub: 1,
 			start: 'top 30%',
-			markers: true,
 		},
 
 		xPercent: -35,
@@ -46,13 +44,84 @@ function mountSectionOne() {
 }
 
 function mountSectionTwo() {
-	timelineSectionTwo = gsap.from('.grid-panel', {
-		scrollTrigger: '.grid-panel',
+	const sectionTwo = document.querySelector('#section-two')
+	const gridPanel = document.querySelector('#section-two .grid-panel')
+	const sectionTitle = document.querySelector(
+		'#section-two .section-title .title'
+	)
+
+	/** ANIMATE LEFT GRID PANEL */
+	gsap.from(gridPanel, {
+		scrollTrigger: sectionTwo,
 		duration: 1,
 		x: -500,
+	})
+
+	/** ANIMATE RIGHT SECTION TITLE */
+	gsap.to(sectionTitle, {
+		scrollTrigger: {
+			trigger: sectionTwo,
+			scrub: true,
+		},
+		yPercent: -80,
+	})
+
+	/**ANIMATE SECTION TWO */
+	/*
+	gsap.to(sectionTwo, {
+		scrollTrigger: {
+			trigger: sectionTwo,
+			scrub: true,
+			start: '20% center',
+		},
+		yPercent: 30,
+	})
+	*/
+}
+
+function mountSectionThree() {
+	const sectionThree = document.querySelector('#section-three')
+	const gridCardsLeft = document.querySelectorAll(
+		'#section-three .grid-panel .left'
+	)
+	const gridCardsRight = document.querySelectorAll(
+		'#section-three .grid-panel .right'
+	)
+	const sectionTitle = document.querySelector(
+		'#section-three .section-title .title'
+	)
+
+	gsap.from(gridCardsLeft, {
+		scrollTrigger: {
+			trigger: sectionThree,
+			scrub: false,
+			start: 'top center',
+		},
+		duration: 1.5,
+		x: -700,
+		skewX: 20,
+		skewY: 30,
+		opacity: 0,
+		ease: 'power2.out',
+		stagger: 0.3,
+	})
+
+	gsap.from(gridCardsRight, {
+		scrollTrigger: {
+			trigger: sectionThree,
+			scrub: false,
+			start: 'top center',
+		},
+		duration: 1.5,
+		x: 700,
+		skewX: -20,
+		skewY: -30,
+		opacity: 0,
+		ease: 'power2.out',
+		stagger: 0.3,
 	})
 }
 
 export function unmountProjects3() {
-	timelineSectionOne = timelineSectionTwo = null
+	timelineSectionOne = null
 }
