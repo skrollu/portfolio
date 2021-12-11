2ccfba0a76a198c8421d1d6f94b899d180e2b3eb
Mathieu Ibersien <mat98-76@live.fr>
2021-12-11 16:42:08 +0100

    Section one animated correctly

--- src/js/projects3.js
+++ src/js/projects3.js
@@ -8,7 +8,42 @@ export function mountProjects3() {
 	mountSectionTwo()
 }
 
-function mountSectionOne() {}
+function mountSectionOne() {
+	const sectionOne = document.querySelector('#section-one')
+	const card = document.querySelector('#section-one .card-container')
+	const scaffoldCards = document.querySelectorAll(
+		'#section-one .scaffold-container .scaffold-card'
+	)
+
+	console.log(scaffoldCards)
+	const timeline = gsap.timeline()
+
+	/**LEFT CARD ANIMATION */
+	timeline.to(card, {
+		scrollTrigger: {
+			trigger: sectionOne,
+			scrub: 0.5,
+			start: 'top 30%',
+			markers: true,
+		},
+
+		yPercent: 50,
+	})
+
+	//scaffoldCards.forEach((element) => {})
+
+	timeline.to(scaffoldCards, {
+		scrollTrigger: {
+			trigger: sectionOne,
+			scrub: 1,
+			start: 'top 30%',
+			markers: true,
+		},
+
+		xPercent: -35,
+		stagger: 0.15,
+	})
+}
 
 function mountSectionTwo() {
 	console.log('gsap')
